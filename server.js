33735febd18f1fb4919c07670a7405701f072549
Arun <arunpatel209@gmail.com>
2020-09-02 17:04:52 +0530

    application name modified in json

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/COVID19'));
+app.use(express.static(__dirname + '/dist/Covid19'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+
-        '/dist/COVID19/index.html'));
+        '/dist/Covid19/index.html'));
 });
 app.listen(process.env.PORT || 8080)
